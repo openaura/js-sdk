9ea79231bb2816ed3a8baf56c09be535f9ef7196
Dean Hudson <dean@ero.com>
2014-06-09 16:32:10 -0700

    Docco task and actual docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+var docco = require("gulp-docco");
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
   // place code for your default task here
   console.log("hello, world!");
 });
 
-// Basic usage
-var uglify = require('gulp-uglify');
-
 gulp.task('compress', function() {
   gulp.src('lib/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('docs', function () {
+  gulp.src("./src/js/*.js")
+    .pipe(docco())
+    .pipe(gulp.dest('./docs'));
 });
 
 gulp.task('package-all', function() {
